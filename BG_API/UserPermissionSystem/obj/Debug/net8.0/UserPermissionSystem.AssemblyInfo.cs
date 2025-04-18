bbeb7b538a50887e55404d40c8d696e7e9c2233b
wuaigrugmmeu <wuaigreugmmeuai@foxmail.com>
2025-04-18 02:30:53 +0000

    12

--- BG_API/UserPermissionSystem/obj/Debug/net8.0/UserPermissionSystem.AssemblyInfo.cs
+++ BG_API/UserPermissionSystem/obj/Debug/net8.0/UserPermissionSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserPermissionSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64c793ee0e3a9b787c381a685207718524263cf8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+832e0fe6c7a3c65d12f3ffe341522be232030eed")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserPermissionSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserPermissionSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
